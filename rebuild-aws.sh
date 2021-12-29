53eca7d23a78df2a92e55c002c6b8af647e61b1e
Alex Chang <alex.chang@unidatum.com>
2021-12-29 11:30:08 -0600

    added aws auth

--- rebuild-aws.sh
+++ rebuild-aws.sh
@@ -3,6 +3,9 @@
 # this is the base url of your AWS ECR (elastic container repository)
 ECR_REPO_BASE_URL=365696073803.dkr.ecr.us-east-1.amazonaws.com
 
+sudo chmod 666 /var/run/docker.sock
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $ECR_REPO_BASE_URL
+
 docker build -t="chang/base" os-base
 docker tag chang/base:latest $ECR_REPO_BASE_URL/storm-base:latest
 docker push $ECR_REPO_BASE_URL/storm-base:latest
