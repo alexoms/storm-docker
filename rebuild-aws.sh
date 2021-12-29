86148d213780feaf6a8e1502ff0e6129860d238a
Alex Chang <alex.chang@unidatum.com>
2021-12-28 18:06:53 -0600

    refactored ecr repo url

--- rebuild-aws.sh
+++ rebuild-aws.sh
@@ -1,29 +1,28 @@
 #!/bin/bash
 
+# this is the base url of your AWS ECR (elastic container repository)
+ECR_REPO_BASE_URL=365696073803.dkr.ecr.us-east-1.amazonaws.com
+
 docker build -t="chang/base" os-base
-docker tag chang/base:latest 365696073803.dkr.ecr.us-east-1.amazonaws.com/storm-base:latest
-docker push 365696073803.dkr.ecr.us-east-1.amazonaws.com/storm-base:latest
+docker tag chang/base:latest $ECR_REPO_BASE_URL/storm-base:latest
+docker push $ECR_REPO_BASE_URL/storm-base:latest
 
 docker build -t="zookeeper" zookeeper
-docker tag zookeeper:latest 365696073803.dkr.ecr.us-east-1.amazonaws.com/zookeeper:latest
-docker push 365696073803.dkr.ecr.us-east-1.amazonaws.com/zookeeper:latest
+docker tag zookeeper:latest $ECR_REPO_BASE_URL/zookeeper:latest
+docker push $ECR_REPO_BASE_URL/zookeeper:latest
 
 docker build -t="chang/storm" storm
-docker tag chang/storm:latest 365696073803.dkr.ecr.us-east-1.amazonaws.com/storm:latest
-docker push 365696073803.dkr.ecr.us-east-1.amazonaws.com/storm:latest
+docker tag chang/storm:latest $ECR_REPO_BASE_URL/storm:latest
+docker push $ECR_REPO_BASE_URL/storm:latest
 
 docker build -t="storm-nimbus" storm-nimbus
-docker tag storm-nimbus:latest 365696073803.dkr.ecr.us-east-1.amazonaws.com/storm-nimbus:latest
-docker push 365696073803.dkr.ecr.us-east-1.amazonaws.com/storm-nimbus:latest
+docker tag storm-nimbus:latest $ECR_REPO_BASE_URL/storm-nimbus:latest
+docker push $ECR_REPO_BASE_URL/storm-nimbus:latest
 
 docker build -t="storm-supervisor" storm-supervisor
-docker tag storm-supervisor:latest 365696073803.dkr.ecr.us-east-1.amazonaws.com/storm-supervisor:latest
-docker push 365696073803.dkr.ecr.us-east-1.amazonaws.com/storm-supervisor:latest
+docker tag storm-supervisor:latest $ECR_REPO_BASE_URL/storm-supervisor:latest
+docker push $ECR_REPO_BASE_URL/storm-supervisor:latest
 
 docker build -t="storm-ui" storm-ui
-docker tag storm-ui:latest 365696073803.dkr.ecr.us-east-1.amazonaws.com/storm-ui:latest
-docker push 365696073803.dkr.ecr.us-east-1.amazonaws.com/storm-ui:latest
-
-
-
-
+docker tag storm-ui:latest $ECR_REPO_BASE_URL/storm-ui:latest
+docker push $ECR_REPO_BASE_URL/storm-ui:latest
