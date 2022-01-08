42877be47932989f2114c2ab56db8d21da74e8c8
Alex Chang <alex.chang@unidatum.com>
2022-01-08 14:56:20 -0600

    port change

--- aws-deploy-topology.sh
+++ aws-deploy-topology.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 
-/data/apache-storm-0.9.2-incubating/bin/storm jar ~/src_chang/storm/examples/storm-starter/target/storm-starter-0.9.8-SNAPSHOT-jar-with-dependencies.jar storm.starter.RollingTopWords production-topology -c nimbus.host=nimbus100.storm -c nimbus.thrift.port=49627
+/data/apache-storm-0.9.2-incubating/bin/storm jar ~/src_chang/storm/examples/storm-starter/target/storm-starter-0.9.8-SNAPSHOT-jar-with-dependencies.jar storm.starter.RollingTopWords production-topology -c nimbus.host=nimbus100.storm -c nimbus.thrift.port=6627
