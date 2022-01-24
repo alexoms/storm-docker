121383e92cb6a7bc7ecc9ff85a9e00ba56f393f6
Alex Chang <alex.chang@unidatum.com>
2022-01-23 20:35:36 -0600

    fixed local reference

--- aws-ecr-rebuild-as-storm-2.3.0.sh
+++ aws-ecr-rebuild-as-storm-2.3.0.sh
@@ -6,26 +6,26 @@ source aws-ecr.properties
 set +a # stop exporting
 
 echo "Using $ECR_REPO_BASE_URL"
-docker build -t="chang/base" storm-2.3.0-cluster/os-base
+docker build -t="chang/base:2.3.0-storm" storm-2.3.0-cluster/os-base
 docker tag chang/base:2.3.0-storm $ECR_REPO_BASE_URL/storm-base:2.3.0-storm
 docker push $ECR_REPO_BASE_URL/storm-base:2.3.0-storm
 
-docker build -t="zookeeper" storm-2.3.0-cluster/zookeeper
+docker build -t="zookeeper:2.3.0-storm" storm-2.3.0-cluster/zookeeper
 docker tag zookeeper:2.3.0-storm $ECR_REPO_BASE_URL/zookeeper:2.3.0-storm
 docker push $ECR_REPO_BASE_URL/zookeeper:2.3.0-storm
 
-docker build -t="chang/storm" storm-2.3.0-cluster/storm
+docker build -t="chang/storm:2.3.0-storm" storm-2.3.0-cluster/storm
 docker tag chang/storm:2.3.0-storm $ECR_REPO_BASE_URL/storm:2.3.0-storm
 docker push $ECR_REPO_BASE_URL/storm:2.3.0-storm
 
-docker build -t="storm-nimbus" storm-2.3.0-cluster/storm-nimbus
+docker build -t="storm-nimbus:2.3.0-storm" storm-2.3.0-cluster/storm-nimbus
 docker tag storm-nimbus:2.3.0-storm $ECR_REPO_BASE_URL/storm-nimbus:2.3.0-storm
 docker push $ECR_REPO_BASE_URL/storm-nimbus:2.3.0-storm
 
-docker build -t="storm-supervisor" storm-2.3.0-cluster/storm-supervisor
+docker build -t="storm-supervisor:2.3.0-storm" storm-2.3.0-cluster/storm-supervisor
 docker tag storm-supervisor:2.3.0-storm $ECR_REPO_BASE_URL/storm-supervisor:2.3.0-storm
 docker push $ECR_REPO_BASE_URL/storm-supervisor:2.3.0-storm
 
-docker build -t="storm-ui" storm-2.3.0-cluster/storm-ui
+docker build -t="storm-ui:2.3.0-storm" storm-2.3.0-cluster/storm-ui
 docker tag storm-ui:2.3.0-storm $ECR_REPO_BASE_URL/storm-ui:2.3.0-storm
 docker push $ECR_REPO_BASE_URL/storm-ui:2.3.0-storm
