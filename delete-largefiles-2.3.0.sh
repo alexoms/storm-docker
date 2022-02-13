a0f768d6a874415ea994bdc43a15334999cb62e5
Alex Chang <alex.chang@unidatum.com>
2022-02-13 23:43:43 +0000

    fixed dockerfiles for no download use case

--- delete-largefiles-2.3.0.sh
+++ delete-largefiles-2.3.0.sh
@@ -4,10 +4,10 @@ export ZOOKEEPER_VERSION=3.7.0
 export STORM_VER=apache-storm-2.3.0
 
 cd storm-2.3.0-cluster-no-dl/zookeeper/
-rm apache-zookeeper-${ZOOKEEPER_VERSION}-bin.tar.gz && \
-rm KEYS && \
-rm apache-zookeeper-${ZOOKEEPER_VERSION}-bin.tar.gz.asc &&>
-rm apache-zookeeper-${ZOOKEEPER_VERSION}-bin.tar.gz.sha512
+rm apache-zookeeper-3.7.0-bin.tar.gz
+rm KEYS
+rm apache-zookeeper-3.7.0-bin.tar.gz.asc
+rm apache-zookeeper-3.7.0-bin.tar.gz.sha512
 cd ../storm/
-rm ${STORM_VER}.tar.gz
+rm apache-storm-2.3.0.tar.gz
 
