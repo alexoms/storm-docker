31b39141f4d387ae45a7d8e88ffed8c075356c50
Alex Chang <alex.chang@unidatum.com>
2022-04-07 23:28:50 -0500

    added draft zookeeper ensemble cluster conf

--- storm-2.3.0-cluster-no-dl/zookeeper/start-zk.sh
+++ storm-2.3.0-cluster-no-dl/zookeeper/start-zk.sh
@@ -1,4 +1,11 @@
 sed -i -r 's|#(log4j.appender.ROLLINGFILE.MaxBackupIndex.*)|\1|g' $ZK_HOME/conf/log4j.properties
 sed -i -r 's|#autopurge|autopurge|g' $ZK_HOME/conf/zoo.cfg
 
+# Configure zookeeper cluster ensemble (https://zookeeper.apache.org/doc/r3.1.2/zookeeperAdmin.html)
+echo 'initLimit=5' >> $ZK_HOME/conf/zoo.cfg
+echo 'syncLimit=2' >> $ZK_HOME/conf/zoo.cfg
+echo 'server.1=zookeeper1.storm:2888:3888' >> $ZK_HOME/conf/zoo.cfg
+echo 'server.2=zookeeper2.storm:2888:3888' >> $ZK_HOME/conf/zoo.cfg
+echo 'server.3=zookeeper3.storm:2888:3888' >> $ZK_HOME/conf/zoo.cfg
+
 $ZK_HOME/bin/zkServer.sh start-foreground
