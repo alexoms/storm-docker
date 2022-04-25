41185047fd2e0921a79e0841fc5c936bbdb34d91
Alex Chang <alex.chang@unidatum.com>
2022-04-25 11:39:21 -0500

    nt

--- storm-2.3.0-cluster-no-dl/zookeeper/start-zk.sh
+++ storm-2.3.0-cluster-no-dl/zookeeper/start-zk.sh
@@ -4,10 +4,7 @@ sed -i -r 's|#autopurge|autopurge|g' $ZK_HOME/conf/zoo.cfg
 # Configure zookeeper cluster ensemble (https://zookeeper.apache.org/doc/r3.1.2/zookeeperAdmin.html)
 echo 'initLimit=5' >> $ZK_HOME/conf/zoo.cfg
 echo 'syncLimit=2' >> $ZK_HOME/conf/zoo.cfg
-echo 'server.1=zookeeper1.storm:2888:3888' >> $ZK_HOME/conf/zoo.cfg
-echo 'server.2=zookeeper2.storm:2888:3888' >> $ZK_HOME/conf/zoo.cfg
-echo 'server.3=zookeeper3.storm:2888:3888' >> $ZK_HOME/conf/zoo.cfg
-echo 'electionPortBindRetry=0' >> $ZK_HOME/conf/zoo.cfg
+#echo 'electionPortBindRetry=0' >> $ZK_HOME/conf/zoo.cfg
 
 # get zookeeper myid from ECS container level properties specified in task definition (env var named ZOOKEEPER_MYID)
 # Will need a zookeeper1 task definition where ZOOKEEPER_MYID=1 in the container env var
@@ -20,5 +17,25 @@ if [ -f "$envmyiddir" ]
 then 
     echo "$ZOOKEEPER_MYID" > "$envmyiddir"
 fi
+
+if [ $ZOOKEEPER_MYID = "1" ];
+then 
+    echo 'server.1=0.0.0.0:2888:3888' >> $ZK_HOME/conf/zoo.cfg
+    echo 'server.2=zookeeper2.storm:2888:3888' >> $ZK_HOME/conf/zoo.cfg
+	echo 'server.3=zookeeper3.storm:2888:3888' >> $ZK_HOME/conf/zoo.cfg
+fi
+if [ $ZOOKEEPER_MYID = "2" ];
+then 
+    echo 'server.1=zookeeper1.storm:2888:3888' >> $ZK_HOME/conf/zoo.cfg
+    echo 'server.2=0.0.0.0:2888:3888' >> $ZK_HOME/conf/zoo.cfg
+	echo 'server.3=zookeeper3.storm:2888:3888' >> $ZK_HOME/conf/zoo.cfg
+fi
+if [ $ZOOKEEPER_MYID = "3" ];
+then 
+    echo 'server.1=zookeeper1.storm:2888:3888' >> $ZK_HOME/conf/zoo.cfg
+    echo 'server.2=zookeeper2.storm:2888:3888' >> $ZK_HOME/conf/zoo.cfg
+	echo 'server.3=0.0.0.0:2888:3888' >> $ZK_HOME/conf/zoo.cfg
+fi
+
 
 $ZK_HOME/bin/zkServer.sh start-foreground
