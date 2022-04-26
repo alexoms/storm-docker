57af0cb2859a9362780490c83deb9ba2418fb339
Alex Chang <alex.chang@unidatum.com>
2022-04-25 21:29:03 -0500

    scripts to speed up ecs task startup

---
+++ aws-ecr-start-cluster.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -a # export all variables created next
+source aws-ecr.properties
+set +a # stop exporting
+
+echo "Using $ECR_REPO_BASE_URL"
+
+aws ecs update-service --region us-east-1 --cluster storm-cluster --service nimbus --desired-count 1
+aws ecs update-service --region us-east-1 --cluster storm-cluster --service zookeeper1 --desired-count 1
+aws ecs update-service --region us-east-1 --cluster storm-cluster --service zookeeper2 --desired-count 1
+aws ecs update-service --region us-east-1 --cluster storm-cluster --service zookeeper3 --desired-count 1
+aws ecs update-service --region us-east-1 --cluster storm-cluster --service storm-supervisor --desired-count 1
+aws ecs update-service --region us-east-1 --cluster storm-cluster --service stormui --desired-count 1
